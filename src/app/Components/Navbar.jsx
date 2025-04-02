482939a1faae875ef9d134f248fb5b93cfd71451
Amr maher <amrm80233@>
2025-04-02 20:45:05 +0200

    feat(Navbar): add search dropdown menu
    Adds a dropdown component to the main navigation bar.
    This dropdown provides quick links for searching actors and movies.
    Styled using Tailwind CSS with hover and focus states for improved UX.

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import AuthButton from "./ui/AuthButton";
@@ -9,11 +11,16 @@ import {
   faFilm,
   faFire,
   faList,
+  faSearch,
+  faChevronDown,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
   const NavData = [
     { title: "Home", link: "/", icon: faHouse },
     { title: "Actors", link: "/Actors", icon: faUsers },
@@ -23,12 +30,21 @@ function Navbar() {
   ];
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
       }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+      if (window.scrollY > 50) setIsDropdownOpen(false);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -87,14 +103,15 @@ function Navbar() {
       animate={isScrolled ? "visible" : "hidden"}
     >
       <div className="container mx-auto flex items-center justify-between">
-        <a href="/" className="text-red-600 text-3xl font-bold">
+        <Link href="/" className="text-red-600 text-3xl font-bold">
           Netflix
-        </a>
+        </Link>
 
         <div className="md:hidden">
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             className="text-white focus:outline-none"
+            aria-label="Toggle menu"
           >
             <svg
               className="h-6 w-6"
@@ -114,40 +131,95 @@ function Navbar() {
         </div>
 
         <div className="hidden md:flex items-center space-x-6 text-white">
-          {NavData.map((ele, i) => (
-            <motion.a
-              key={i}
-              href={`${ele.link}`}
-              className="hover:text-gray-300"
-              variants={linkVariants}
-              whileHover="hover"
-            >
-              {ele.title}
-            </motion.a>
+          {NavData.map((ele) => (
+            <Link href={ele.link} key={ele.link} legacyBehavior passHref>
+              <motion.a
+                className="hover:text-gray-300"
+                variants={linkVariants}
+                whileHover="hover"
+              >
+                {ele.title}
+              </motion.a>
+            </Link>
           ))}
         </div>
 
-        <div className="flex items-center space-x-4 text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 hover:text-gray-300 cursor-pointer"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+        <div
+          className="flex items-center space-x-4 text-white relative"
+          ref={dropdownRef}
+        >
+          <button
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            className="text-white font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center hover:text-gray-300"
+            type="button"
+            aria-haspopup="true"
+            aria-expanded={isDropdownOpen}
+            aria-label="User options"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            <FontAwesomeIcon icon={faSearch} className="h-5 w-5 text-md" />
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              className="w-2.5 h-2.5 ms-2"
             />
-          </svg>
+          </button>
+
+          {isDropdownOpen && (
+            <div
+              className="
+    absolute top-full right-0 mt-2 z-50 w-56
+    origin-top-right
+    rounded-md
+   bg-black
+    shadow-lg
+    ring-1 ring-black ring-opacity-5
+    focus:outline-none
+  "
+            >
+              <ul className="py-1" aria-labelledby="dropdownHoverButton">
+                <li>
+                  <Link
+                    href="/dashboard"
+                    className="
+          group flex items-center px-4 py-2 text-sm
+          text-gray-700 dark:text-gray-300
+          hover:bg-red-600 hover:text-white
+          dark:hover:bg-red-700
+          transition-colors duration-150 ease-in-out
+          focus:outline-none focus:bg-red-600 focus:text-white
+          dark:focus:bg-red-700
+        "
+                    onClick={() => setIsDropdownOpen(false)}
+                  >
+                    Search Actors
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/settings"
+                    className="
+          group flex items-center px-4 py-2 text-sm
+          text-gray-700 dark:text-gray-300
+          hover:bg-red-600 hover:text-white
+          dark:hover:bg-red-700
+          transition-colors duration-150 ease-in-out
+          focus:outline-none focus:bg-red-600 focus:text-white
+          dark:focus:bg-red-700
+        "
+                    onClick={() => setIsDropdownOpen(false)}
+                  >
+                    Search Movies
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
+
           <AuthButton ButtonText={"Sign in"} ButtonLink={"Login"} />
         </div>
       </div>
 
       <motion.div
-        className="fixed top-0 left-0 h-full w-64 bg-black p-4 z-50 md:hidden"
+        className="fixed top-0 left-0 h-full w-fit bg-black p-4 z-[60] md:hidden"
         variants={mobileMenuVariants}
         animate={isMenuOpen ? "open" : "closed"}
         initial="closed"
@@ -155,6 +227,7 @@ function Navbar() {
         <button
           onClick={() => setIsMenuOpen(false)}
           className="text-white absolute top-4 right-4 focus:outline-none"
+          aria-label="Close menu"
         >
           <svg
             className="h-6 w-6"
@@ -172,17 +245,15 @@ function Navbar() {
           </svg>
         </button>
         <div className="flex flex-col space-y-4 mt-12">
-          {NavData.map((ele, i) => (
+          {NavData.map((ele) => (
             <Link
-              href={`${ele.link}`}
-              key={i}
-              className="text-white  p-2 rounded flex items-center justify-start gap-1 hover:bg-red-700 hover:text-white"
+              href={ele.link}
+              key={ele.link}
+              className="text-white p-2 rounded flex items-center justify-start gap-2 hover:bg-red-700 hover:text-white"
               onClick={() => setIsMenuOpen(false)}
             >
-              <>
-                <FontAwesomeIcon icon={ele.icon} />
-                {ele.title}
-              </>
+              <FontAwesomeIcon icon={ele.icon} className="w-5 h-5" />
+              {ele.title}
             </Link>
           ))}
           <AuthButton ButtonText={"Log out"} ButtonLink={"Sign"} />
