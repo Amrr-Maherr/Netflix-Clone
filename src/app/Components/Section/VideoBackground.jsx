dd957beceefea6d161ab74fc4fb6a674b33cae3a
Amr maher <amrm80233@>
2025-04-08 23:20:42 +0200

    add footer

--- src/app/Components/Section/VideoBackground.jsx
+++ src/app/Components/Section/VideoBackground.jsx
@@ -5,7 +5,7 @@ function VideoBackground() {
       "/Assets/Netflix Intro 1080p (Highest Quality)(1080P_HD).mp4";
     
   return (
-    <div className="fixed top-0 left-0 w-full h-screen -z-10 overflow-hidden">
+    <div className="w-full h-screen -z-10 overflow-hidden">
       <video
         autoPlay
         loop
--- src/app/Movies/page.jsx
+++ src/app/Movies/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import VideoBackground from "../Components/Section/VideoBackground";
+import Footer from "../Components/Footer/page";
 
 const videoSource = "/Assets/Netflix Intro 1080p (Highest Quality)(1080P_HD).mp4";
 
@@ -10,6 +11,7 @@ export default function Movies() {
     <>
     <Navbar/>
     <VideoBackground/>
+    <Footer/>
     </>
   );
 }
