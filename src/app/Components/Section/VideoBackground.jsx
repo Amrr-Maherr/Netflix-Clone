22986b29870cc4a686833e101fea920fb39fec7d
Amr maher <amrm80233@>
2025-04-08 23:41:15 +0200

    add npx shadcn@latest init

--- src/app/Components/Section/VideoBackground.jsx
+++ src/app/Components/Section/VideoBackground.jsx
@@ -5,13 +5,13 @@ function VideoBackground() {
       "/Assets/Netflix Intro 1080p (Highest Quality)(1080P_HD).mp4";
     
   return (
-    <div className="w-full h-screen -z-10 overflow-hidden">
+    <div className=" w-full h-screen  overflow-hidden">
       <video
         autoPlay
         loop
         muted
         playsInline
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full w-auto h-auto object-cover"
+        className="min-w-full min-h-full w-auto h-auto object-cover"
       >
         <source src={videoSource} type="video/mp4" />
         Your browser does not support the video tag.
--- src/app/Movies/page.jsx
+++ src/app/Movies/page.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+// تأكد من أن هذا المسار صحيح إذا كنت ستستخدمه لاحقاً
 import VideoBackground from "../Components/Section/VideoBackground";
-import Footer from "../Components/Footer/page";
+import Footer from "../Components/Footer/page"; // تأكد من مسار Footer
+import { Button } from "@/components/ui/button"; // تأكد من مسار Button
 
+// تأكد من مسار الفيديو إذا كنت ستستخدمه
 const videoSource = "/Assets/Netflix Intro 1080p (Highest Quality)(1080P_HD).mp4";
-
 
 export default function Movies() {
   return (
     <>
-    <Navbar/>
-    <VideoBackground/>
-    <Footer/>
+      <Navbar />
+      <VideoBackground src={videoSource} />
+      <Footer />
     </>
   );
 }
