8af08c2c594e38236ac60f05ebe607f0bffc0162
Amr maher <amrrmaherr.24@gmail.com>
2025-05-11 23:19:33 +0300

    Enhance VideoBackground performance by adding preload and poster attributes

--- src/app/Components/Section/VideoBackground.jsx
+++ src/app/Components/Section/VideoBackground.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-
+import AltImg from "../../../../public/Assets/Netflix_Logo_PMS.png"
 function VideoBackground({ children, src }) {
   return (
     <div className="relative w-full h-screen overflow-hidden">
+      {/* Video Element */}
       <video
         autoPlay
         loop
         muted
         playsInline
+        preload="auto"
+        poster={AltImg.src}
         className="min-w-full min-h-full w-auto h-auto object-cover"
       >
         <source src={src} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      {/* Overlay Content */}
       <div className="absolute inset-0">{children}</div>
     </div>
   );
