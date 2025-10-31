244795d421e45f26d3f0e9e7c2622a5fdebbf2f6
Amrr-Maherr <amrr.maherr242gmail.com>
2025-10-31 20:38:02 +0200

    update

--- app/Components/HeroSection/index.tsx
+++ app/Components/HeroSection/index.tsx
@@ -1,6 +1,6 @@
 export default function index() {
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className="relative w-full h-screen overflow-hidden bg-black">
       <video
         className="absolute inset-0 w-full h-full object-cover"
         src="/Videos/NetflixLogo.mp4"
