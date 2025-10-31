db88135144b86ccad3dd8a9580d7e1a0e07d4b27
Amrr-Maherr <amrr.maherr242gmail.com>
2025-10-31 20:34:23 +0200

    (feat): Add responsive container utility class
    - Created a global `.container` class for consistent layout structure
    - Applied full width with centered alignment using `w-full` and `mx-auto`
    - Added responsive horizontal padding for different screen sizes (`px-4` → `px-12`)
    - Defined maximum widths for each breakpoint (`max-w-xs` to `max-w-7xl`)
    - Ensures uniform and adaptive spacing across all pages

--- app/Components/Header/index.tsx
+++ app/Components/Header/index.tsx
@@ -2,7 +2,7 @@ import Logo from "../Logo/index";
 import HeaderLinks from "./elements/HeaderLinks";
 export default function index() {
   return (
-    <header className="w-full">
+    <header className="container">
       <nav className="flex items-center justify-start gap-5 fixed z-30 top-0">
         <Logo />
         <HeaderLinks/>
