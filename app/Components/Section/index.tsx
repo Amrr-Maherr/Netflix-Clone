c54d8772f1bda80bc9f1b3bc5f4b579a0d33a9b4
Amrr-Maherr <amrr.maherr242gmail.com>
2025-11-01 20:36:25 +0200

    feat: add rotating text animation component and update section component

--- app/Components/Section/index.tsx
+++ app/Components/Section/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DataList from "./DataList";
 import SplitText from "../Animations/SplitText";
+import RotatingText from "../Animations/RotatingText";
 
 type Movie = {
   id: number;
@@ -11,18 +12,52 @@ type Movie = {
 type IndexProps = {
   Data: Movie[];
   title: string;
+  animationText?: [];
   isMovie:boolean
 };
 
-export default function Index({ Data, title, isMovie }: IndexProps) {
+export default function Index({ Data, title, isMovie, animationText }: IndexProps) {
+  console.log("re");
+
   return (
     <section className="container">
-      <div className="my-10">
+      <div className="my-10 flex items-center justify-end flex-row-reverse gap-2">
+        <RotatingText
+          texts={[
+            "Trending",
+            "Popular",
+            "Top Rated",
+            "Must Watch",
+            "New",
+            "Hot",
+            "Blockbuster",
+            "Binge",
+            "Epic",
+            "Critics' Choice",
+            "Fan Favorite",
+            "Now Showing",
+            "Action",
+            "Drama",
+            "Comedy",
+            "Thriller",
+            "Adventure",
+            "Unmissable",
+          ]}
+          mainClassName="text-3xl md:text-5xl font-bold mb-4 text-white text-start!"
+          staggerFrom={"random"}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-120%" }}
+          staggerDuration={0.025}
+          splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+          transition={{ type: "spring", damping: 30, stiffness: 200 }}
+          rotationInterval={2000}
+        />
         <SplitText
           text={title || "title"}
-          className="text-3xl md:text-5xl font-bold mb-4 text-white text-start md:text-center"
+          className="text-3xl md:text-5xl font-bold mb-4 text-white text-start!"
           delay={100}
-          duration={0.6}
+          duration={0.2}
           ease="power3.out"
           splitType="chars"
           from={{ opacity: 0, y: 40 }}
