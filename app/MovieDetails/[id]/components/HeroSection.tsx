08554bb9f1195275eb7925cbe2538508d98abf12
Amrr-Maherr <amrr.maherr242gmail.com>
2025-11-10 14:24:36 +0200

    update

--- app/MovieDetails/[id]/components/HeroSection.tsx
+++ app/MovieDetails/[id]/components/HeroSection.tsx
@@ -139,7 +139,7 @@ export default function HeroSection({
               )}
               <button
                 onClick={() => setIsMute(isMute === 1 ? 0 : 1)}
-                className="absolute bottom-0 right-4 md:right-6 z-999 cursor-pointer bg-black/50 text-white p-3 rounded-full hover:bg-black/70 transition flex items-center justify-center"
+                className="z-999 cursor-pointer bg-black/50 text-white p-3 rounded-full hover:bg-black/70 transition flex items-center justify-center"
               >
                 {isMute ? (
                   <VolumeX className="w-6 h-6 cursor-pointer" />
