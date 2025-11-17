f09ac6f1893148fa235a2470d44f89415c6a7a1a
Amrr-Maherr <amrr.maherr242gmail.com>
2025-11-17 12:11:33 +0200

    (feat): Update FetchPopularMovies to include error handling and language support

--- Api/FetchPopularMovies.tsx
+++ Api/FetchPopularMovies.tsx
@@ -1,15 +1,15 @@
 import axios from "axios";
-const fetchMovies = async ({ url = "/movie/popular" }) => {
-  const response = await axios.get(
-    (`https://api.themoviedb.org/3${url}`),
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
-      },
-    }
-  );
+const fetchMovies = async ({ url = "/movie/popular", lang = "en" }) => {
+  const response = await axios.get(`https://api.themoviedb.org/3${url}`, {
+    params: {
+      language: lang,
+    },
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
+    },
+  });
   console.log(response.data.results, "response.data.results");
-  
+
   return response.data.results;
 };
 
